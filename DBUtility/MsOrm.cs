ed6fd0d1e9e2ad20a349854ee9ba59340bd6a200
halyhuang1991 <18807898837@139.com>
2018-10-22 09:53:14 +0800

    1

--- DBUtility/MsOrm.cs
+++ DBUtility/MsOrm.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Linq;
 namespace GenModels.DBUtility
 {
     public class MsOrm
@@ -31,8 +31,43 @@ namespace GenModels.DBUtility
             string v_sql=stringBuilder.ToString();
             return v_sql;
         }
-        public bool insert<T>(T tclass)where T: class{
+        private string GetUpdSql<T>(T tclass,string keys)where T: class{
+            Type t=tclass.GetType();
+            string tablename=t.Name;
+            StringBuilder stringBuilder = new StringBuilder();
+            string[] arrKeys=keys.ToUpper().Split(',');
+            stringBuilder.Append("UPDATE " + tablename + " SET ");
+            List<string> list = new List<string>();
+            List<string> ls = new List<string>();
+            foreach (var p in t.GetProperties())
+            {
+                 var type=p.PropertyType.FullName.ToString().ToLower();
+                       var val=p.GetValue(tclass,null).ToString().Replace("'","''");
+                       if(type.Contains("decimal")){
+                           list.Add(p.Name+"="+val);
+                            if(arrKeys.Contains<string>(p.Name)){
+                                ls.Add(p.Name+"="+val);
+                            }
+                       }else{
+                            list.Add(p.Name+"='"+val+"'");
+                            if(arrKeys.Contains<string>(p.Name)){
+                                ls.Add(p.Name+"='"+val+"'");
+                            }
+                       }
+            }
+            string tmp1=String.Join(",",list);
+            string tmp2=String.Join(",",ls);
+            stringBuilder.Append(tmp1+" where "+tmp2);
+             string v_sql=stringBuilder.ToString();
+            return v_sql;
+        }
+        public bool Insert<T>(T tclass)where T: class{
             string v_sql=GetInsertSql<T>(tclass);
+            int ret=MsDB.ExecuteSql(v_sql);
+            return true;
+        }
+        public bool Update<T>(T tclass,string keys)where T: class{
+            string v_sql=GetUpdSql<T>(tclass,keys);
             int ret=MsDB.ExecuteSql(v_sql);
             return true;
         }
